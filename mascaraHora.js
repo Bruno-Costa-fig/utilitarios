a9e3163aba1eb87def9a3c1f3c9d474b6567e5d6
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2023-05-25 10:07:32 -0300

    Create mascaraHora.js

---
+++ mascaraHora.js
@@ -0,0 +1,16 @@
+//adiciona mascara a Hora
+function MascaraHora(hora) {
+  const value = hora.value
+  value.replace(":", "")
+
+  let horaPadrao = value.padStart(2, '0');
+
+  if (horaPadrao.length < 4) {
+          horaPadrao = horaPadrao.padEnd(4, '0')
+  }
+
+  const horaVal = horaPadrao.substring(0, 2)
+  const minVal = horaPadrao.substring(2)
+
+      document.getElementById("nmHoraCirurgia").value = `${horaVal}:${minVal}`;
+  }
