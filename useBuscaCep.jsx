da3daf7ca7af4e7e88ec6f71d4e344709a8f4706
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2023-06-07 17:43:28 -0300

    Update useBuscaCep.jsx

--- useBuscaCep.jsx
+++ useBuscaCep.jsx
@@ -1,13 +1,23 @@
+import axios from "axios"
+
 async function useBuscaCep(cep) {
   let data = {}
   let error = null
 
-  await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then((res) => res.json())
-    .then((res) => data = res)
-    .catch((err) => {
-      error = err
-    })
+   try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+      data = response.data
+
+    } catch (err){
+      error = err.message
+    }
+
+  // await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  //   .then((res) => res.json())
+  //   .then((res) => data = res)
+  //   .catch((err) => {
+  //     error = err
+  //   })
 
   return { data, error }
 }
