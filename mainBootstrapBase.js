c92743ec776b74ffc0555974cc2510f88828fd28
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2023-08-17 17:36:36 -0300

    Create mainBootstrapBase.js

---
+++ mainBootstrapBase.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+createApp(App).use(bootstrap).mount('#app')
